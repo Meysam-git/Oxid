9f2e7c33c94e91a792d7ba6e4ae998fb035c6591
Meysam <meysam2w@gmail.com>
2019-09-20 13:33:33 +0430

    'oxid'

---
+++ crypto.py
@@ -0,0 +1,6 @@
+import requests
+import sys
+
+response = requests.get(f'https://api.coinmarketcap.com/v1/ticker/{sys.argv[1]}/').json()
+print(response[0]['price_usd'])
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Oxid VPN
+## How to Setup **Iphone**
+### 1-go to setting
+
+```bash
+pip install
+```
+```python
+import requests
+print('set')
+```
+
+-For Test/|\
